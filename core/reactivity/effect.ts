3b8bb042e8a2f35ec81f86fbc86a50e4131ec848
Jindx <390447183@qq.com>
2023-10-05 08:31:41 +0800

    feat: effect runner

--- core/reactivity/effect.ts
+++ core/reactivity/effect.ts
@@ -1,20 +1,22 @@
 class reactiveEffect {
     private _fn: any
 
-    constructor(fn) {
+    constructor(fn, public scheduler?) {
         this._fn = fn
     }
 
     run() {
         activeEffect = this
-        this._fn()
+        return this._fn()
     }
 }
 
-export function effect(fn) {
-    const _effect = new reactiveEffect(fn)
+export function effect(fn, options: any = {}) {
+    const _effect = new reactiveEffect(fn, options.scheduler)
 
     _effect.run()
+
+    return _effect.run.bind(_effect)
 }
 
 let activeEffect;
@@ -45,7 +47,11 @@ export function trigger(target, key) {
     const dep = depsMap.get(key)
 
     dep.forEach((effect) => {
-        effect.run()
+        if (effect.scheduler) {
+            effect.scheduler()
+        } else {
+            effect.run()
+        }
     })
 }
 
--- core/reactivity/tests/effect.spec.ts
+++ core/reactivity/tests/effect.spec.ts
@@ -16,5 +16,44 @@ describe("effect", () => {
         // update
         user.age++;
         expect(nextAge).toBe(12);
+    });
+
+    it('should return runner when call effect', () => {
+        // runner  运行effect -> runner函数 -> 运行fn , 返回fn return值
+        let foo = 1;
+        const runner = effect(() => {
+            foo++
+            return 'foo'
+        })
+
+        expect(foo).toBe(2)
+        const r = runner()
+        expect(foo).toBe(3)
+        expect(r).toBe('foo')
+    });
+
+    it('scheduler', () => {
+        let dummy;
+        let run: any;
+        const obj = reactive({ foo: 1 })
+        const scheduler = jest.fn(() => {
+            run = runner
+        })
+
+        const runner = effect(
+            () => {
+            dummy = obj.foo;
+            },
+            { scheduler }
+        )
+
+        expect(scheduler).not.toHaveBeenCalled()
+        expect(dummy).toBe(1);
+        obj.foo++
+        expect(dummy).toBe(1)
+        expect(scheduler).toBeCalledTimes(1)
+        run()
+        expect(dummy).toBe(2)
+
     })
 });
