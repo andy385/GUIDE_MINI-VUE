ce23cf117a09f9abeecf8592d1d552fe72e49492
jindx <390447183@qq.com>
2023-10-06 14:25:44 +0800

    feat: isRef, unRef

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,5 +1,6 @@
-import { ref } from '../ref';
+import { isRef, ref, unRef } from '../ref';
 import { effect } from '../effect';
+import { reactive } from '../reactive';
 
 describe('ref', () => { 
   it('happy path', () => {
@@ -42,4 +43,18 @@ describe('ref', () => {
     a.value.count++
     expect(dummy).toBe(2)
   });
+
+  it('isRef', () => {
+    const a = ref(1)
+    const b = reactive({ a: 1 })
+    expect(isRef(a)).toBe(true)
+    expect(isRef(1)).toBe(false)
+    expect(isRef(b)).toBe(false)
+  })
+
+  it('unRef', () => {
+    const a = ref(1)
+    expect(unRef(a)).toBe(1)
+    expect(a.value).toBe(1)
+  })
  })
