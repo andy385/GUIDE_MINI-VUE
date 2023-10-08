ddaafd6929804c759e88a4262483f1b4c2de7aac
Jindx <390447183@qq.com>
2023-10-08 23:04:04 +0800

    feat: processElement

---
+++ src/runtime-core/createApp.ts
@@ -0,0 +1,14 @@
+import { render } from "./renderer"
+import { createVNode } from "./vnode"
+
+export function createApp(rootComponent) {
+    return {
+        mount(rootContainer) {
+            // 先转换成vnode
+            // 后续操作都基于vnode
+            const vnode = createVNode(rootComponent)
+
+            render(vnode, rootContainer)
+        }
+    }
+}
