437c99b704004c93169ae8cee2a8290238348933
Jindx <390447183@qq.com>
2023-10-14 00:00:50 +0800

    feat: shapeFlags

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,4 +1,4 @@
-import { isObject } from "../shared/index"
+import { ShapeFlags } from "../shared/shapeFlag"
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode, container) {
@@ -7,10 +7,12 @@ export function render(vnode, container) {
 
 function patch(vnode, container) {
 
-    if (typeof vnode.type === 'string') {
+    const { shapeFlag } = vnode;
+
+    if (shapeFlag & ShapeFlags.ELEMENT) {
         // 处理element
         processElement(vnode, container)
-    } else if (isObject(vnode.type)) {
+    } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
         // 去处理组件
         processComponent(vnode, container)
     }
@@ -30,11 +32,11 @@ function mountElement(vnode, container) {
         el.setAttribute(key, value);
     }
 
-    const { children } = vnode;
+    const { children, shapeFlag } = vnode;
 
-    if (typeof children === 'string') {
+    if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
         el.textContent = children
-    } else if (Array.isArray(children)) {
+    } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
         mountChildren(children, el)
     }
 
