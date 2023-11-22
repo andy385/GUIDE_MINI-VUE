f5478de73aabbd8b68f6192e8faff6d76049453b
Jindx <390447183@qq.com>
2023-11-22 11:44:05 +0800

    feat: transform string

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,11 +1,14 @@
-import { NodeTypes } from "./ast"
-
-export function transform(root, options) {
+export function transform(root, options = {}) {
     const context = createTransformContext(root, options)
     // 遍历-深度优先搜索
     traverseNode(root, context)
-    // 修改text
 
+    createCodegenNode(root)
+
+}
+
+function createCodegenNode(root: any) {
+    root.codegenNode = root.children[0]
 }
 
 function createTransformContext(root: any, options: any) {
